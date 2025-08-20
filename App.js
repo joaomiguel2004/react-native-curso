873fda86edc0a057a74d473019a0544be51e4a8e
João Miguel <joaomiguel-2004@hotmail.com>
2025-08-19 22:04:15 -0300

    Conceito básico de variáveis e estilização

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  render(){
+
+    let nome = 'Matheus';
+    let img = 'https://sujeitoprogramador.com/steve.png'
+
+    return(
+      <View>
+        <Text>Olá, mundo!</Text>
+        <Text>Meu primeiro App</Text>
+        <Text style={{color: '#ff0000', fontSize: 25, margin: 15}}>
+          Sujeito Programador
+        </Text>
+
+        <Image 
+          source={{ uri: img}}
+          style={{width: 300, height: 300}}
+        />
+
+        <Text style={{fontSize: 30}}> {nome} </Text>
+
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
